777bd06d2f50f1eefbd3cbb2b014354e4e4f53c7
elsayed hussen <elsayed.hussen@gmail.com>
2021-11-16 22:52:58 +0200

    gtfdg

--- Answer/04/notes-app/app.js
+++ Answer/04/notes-app/app.js
@@ -3,8 +3,58 @@ const yargs = require("yargs");
 // const validator = require("validator");
 const getNotes = require("./notes.js");
 
-console.log(process.argv);
-console.log(yargs.argv);
+// customize yargs version
+// yargs.version("1.1.0");
+
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note ",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+    // console.log("adding a new note!",argv);
+  },
+});
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note ",
+  handler: function () {
+    console.log("removing the note!");
+  },
+});
+// create list command
+yargs.command({
+  command: "list",
+  describe: "list the notes ",
+  handler: function () {
+    console.log("listing the notes!");
+  },
+});
+// create read command
+yargs.command({
+  command: "read",
+  describe: "read the note ",
+  handler: function () {
+    console.log("reading the note!");
+  },
+});
+yargs.parse();
+// console.log(yargs.argv);
+// console.log(process.argv);
 
 // const command = process.argv[2]
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 npm install -g nodemon@1.18.5
+
+
+nodemon windows fix {
+Run Powershell as Admin, and then:
+
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
