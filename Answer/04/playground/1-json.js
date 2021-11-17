2f4ab745eb32b1f40609d71d22d14f54a7bf418f
elsayed-hussen <elsayed.hussen@gmail.com>
2021-11-17 14:15:20 +0200

    json

--- Answer/04/playground/1-json.js
+++ Answer/04/playground/1-json.js
@@ -1,11 +1,19 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataJSON = dataBuffer.toString()
-const user = JSON.parse(dataJSON)
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
 
-user.name = 'Gunther'
-user.age = 54
+// const book = {
+//   title: "My life story",
+//   author: "elsayed hussen",
+// };
 
-const userJSON = JSON.stringify(user)
-fs.writeFileSync('1-json.json', userJSON)
+// const bookJSON = JSON.stringify(book);
+
+// fs.writeFileSync("1-json.json", bookJSON);
+// console.log(bookJSON);
+
+// const parsedData = JSON.parse(bookJSON)
+// console.log(parsedData.author);
