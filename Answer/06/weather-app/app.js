cfb5ec2670cd43c013003a84e2b2666e13ae3a7e
elsayed hussen <elsayed.hussen@gmail.com>
2021-11-18 14:42:06 +0200

    conict to api

--- Answer/06/weather-app/app.js
+++ Answer/06/weather-app/app.js
@@ -1,11 +1,11 @@
-console.log("starting");
+const request = require("postman-request");
 
-setTimeout(() => {
-  console.log("2 sec");
-}, 2000);
+const url =
+  "http://api.weatherstack.com/current?access_key=b547593da728f465f39132fcafbd67c5&query=New%20York";
 
-setTimeout(() => {
-  console.log("0 sec");
-}, 0);
-
-console.log("ending");
+request(url, (error, response) => {
+  console.log("error:", error);
+  console.log("don");
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
