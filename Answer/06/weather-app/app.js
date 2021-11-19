79544164fd4e5e923b77495e83d25fb18c63647d
elsayed-hussen <elsayed.hussen@gmail.com>
2021-11-19 13:40:13 +0200

    002221

--- Answer/06/weather-app/app.js
+++ Answer/06/weather-app/app.js
@@ -3,9 +3,7 @@ const request = require("postman-request");
 const url =
   "http://api.weatherstack.com/current?access_key=b547593da728f465f39132fcafbd67c5&query=New%20York";
 
-request(url, (error, response) => {
+request({ url: url, json: true }, (error, response) => {
   console.log("error:", error);
-  console.log("don");
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+  console.log(response.body.current);
 });
