f96ac9fdf16fe787e2767cab357f92714f1c8f3a
elsayed hussen <elsayed.hussen@gmail.com>
2021-11-24 00:19:58 +0200

    10 don

---
+++ Answer/11/task-manager/mongodb.js
@@ -0,0 +1,17 @@
+// CRUD create read update delete
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database!");
+    }
+    const db = client.db(databaseName);
+  }
+);
