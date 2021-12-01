c9a1dc8415a031e418d6ae01d3edcfdb5266e953
elsayed hussen <elsayed.hussen@gmail.com>
2021-12-01 20:20:46 +0200

    ggggg

--- Answer/12/task-manager/src/routers/user.js
+++ Answer/12/task-manager/src/routers/user.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
+
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
-
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.get("/users", async (req, res) => {
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/users", auth, async (req, res) => {
   try {
     const users = await User.find({});
     res.send(users);
@@ -48,12 +63,9 @@ router.patch("/users/:id", async (req, res) => {
   if (!isValidOperation) {
     return res.status(400).send({ error: "Invalid updates!" });
   }
-
   try {
     const user = await User.findById(req.params.id);
-    updates.forEach((update) => {
-      user[update] = req.body[update];
-    });
+    updates.forEach((update) => (user[update] = req.body[update]));
     await user.save();
     if (!user) {
       return res.status(404).send();
@@ -63,7 +75,6 @@ router.patch("/users/:id", async (req, res) => {
     res.status(400).send(e);
   }
 });
-
 router.delete("/users/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
