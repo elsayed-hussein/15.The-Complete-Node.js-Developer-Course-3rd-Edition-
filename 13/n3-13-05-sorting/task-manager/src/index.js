cfbce11795680e63a2f12745a281aae40ceaab63
elsayed hussen <elsayed.hussen@gmail.com>
2021-12-14 15:03:28 +0200

    12 don

--- 13/n3-13-05-sorting/task-manager/src/index.js
+++ 13/n3-13-05-sorting/task-manager/src/index.js
@@ -30,13 +30,13 @@ const Task = require('./models/task')
 const User = require('./models/user')
 
 const main = async () => {
-    // const task = await Task.findById('61b88a1e47efcc1efc6dbb2b')
+    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
     // await task.populate('owner').execPopulate()
     // console.log(task.owner)
- 
-    const user = await User.findById('61b88a1e47efcc1efc6dbb2b')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
+
+    // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+    // await user.populate('tasks').execPopulate()
+    // console.log(user.tasks)
 }
 
 main()
